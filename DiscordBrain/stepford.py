79804e5403cbd31ba0d7f7bd6ae148a9f150cb5b
Andrew Bryar <abryar89@gmail.com>
2020-11-11 01:11:14 -0600

    Added Pong

--- DiscordBrain/stepford.py
+++ DiscordBrain/stepford.py
@@ -1,17 +1,30 @@
 import discord 
-
-
-
 from discord.ext import commands
-
 
 # End of Script we run...
 TOKEN = open("token.txt","r").readline()
-client = commands.Bot(command_prefix = '$')
+
+bot = commands.Bot(command_prefix = '$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('Back to work, sir.')
+    print('--------------------')
 
 # answers with the ms latency
-@client.command()
+#TEST COMMANDS
+@bot.command()
 async def ping(ctx):
-    await ctx.send(f'PONG! {round (client.latency * 1000)} ms')
+    await ctx.send(f'PONG! {round (bot.latency * 1000)} ms')
 
-client.run(TOKEN)
+@bot.command()
+async def pong(ctx):
+    await ctx.send(f'PING! {round (bot.latency * 1000)} ms')
+
+
+
+
+bot.run(TOKEN)
