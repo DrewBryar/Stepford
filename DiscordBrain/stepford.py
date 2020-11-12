b58a5bd54978f6963b72711553b4fe339fbbc5bd
DrewBryar <abryar89@gmail.com>
2020-11-12 03:03:57 -0600

    ttsx3 didn't work with certain Python versions. X

--- DiscordBrain/stepford.py
+++ DiscordBrain/stepford.py
@@ -1,21 +1,12 @@
 import discord 
 from discord.ext import commands
-import pyttsx3
 # End of Script we run...
 TOKEN = open("token.txt","r").readline()
 
-bot = commands.Bot(command_prefix = '$')
+bot = commands.Bot(command_prefix = 'Stepford, please ')
 
 # TTS______________________________________________________
-# https://pypi.org/project/pyttsx3/
-# >> pip install pyttsx3
-engine = pyttsx3.init()
-rate = engine.getProperty('rate')
-volume = engine.getProperty('volume')
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id) #The variable in voices represents a male voice (0) and a female voice (1).
-# Maybe pursue new voices? They seem to be system dependent.
 # __________________________________________________________
 
 # Speech Recognition________________________________________
@@ -31,15 +22,11 @@ async def on_ready():
     print(bot.user.id)
     print('Back to work, sir.')
     print('--------------------')
-    engine.say("Ah, good to see you sir.")
-    engine.runAndWait()
 
 # answers with the ms latency
 #TEST COMMANDS
 @bot.command()
 async def ping(ctx):
-    engine.say("PONG PONG PONG, sir.")
-    engine.runAndWait()
     await ctx.send(f'PONG! {round (bot.latency * 1000)} ms')
 
 @bot.command()
